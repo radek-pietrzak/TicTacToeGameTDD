1e26fdd2469ed13c9aadd230ed4a5188db1c212e
radek <radekpietrzak102@gmail.com>
2021-11-22 08:10:10 +0100

    fill in matrix

--- src/main/java/org/tictactoe/game/map/GameMap.java
+++ src/main/java/org/tictactoe/game/map/GameMap.java
@@ -1,14 +1,28 @@
 package org.tictactoe.game.map;
 
+import org.tictactoe.game.matrix.ActorMatrix;
+
 public class GameMap {
 
-    public void createGameMap() {
-        System.out.print("""
-                 | | 
-                -+-+-
-                 | | 
-                -+-+-
-                 | | 
-                """);
+    public void createGameMap(ActorMatrix actorMatrix) {
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                sb.append(actorMatrix.getMatrix()[i][j]);
+                if (j < 2)
+                    sb.append("|");
+                else if (i < 2)
+                    sb.append("\n");
+            }
+            if (i < 2)
+                sb.append("-+-+-\n");
+        }
+
+        System.out.print(sb);
+
     }
+
+
 }
--- src/test/java/org/tictactoe/game/map/GameMapTest.java
+++ src/test/java/org/tictactoe/game/map/GameMapTest.java
@@ -3,6 +3,7 @@ package org.tictactoe.game.map;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.tictactoe.game.matrix.ActorMatrix;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -30,16 +31,64 @@ public class GameMapTest {
 
         //given
         GameMap gameMap = new GameMap();
+        ActorMatrix actorMatrix = new ActorMatrix();
+
         String emptyMap = """
-                 | | 
+                 | |\s
                 -+-+-
-                 | | 
+                 | |\s
                 -+-+-
-                 | | 
-                """;
+                 | |\s""";
 
         //when
-        gameMap.createGameMap();
+        gameMap.createGameMap(actorMatrix);
+
+        //then
+        assertEquals(emptyMap, outputStreamCaptor.toString());
+    }
+
+    @Test
+    void shouldPrintMapWithOInMiddle() {
+
+        //given
+        GameMap gameMap = new GameMap();
+        ActorMatrix actorMatrix = new ActorMatrix();
+        actorMatrix.addActorToMatrix(false, 22);
+
+        String emptyMap = """
+                 | |\s
+                -+-+-
+                 |O|\s
+                -+-+-
+                 | |\s""";
+
+
+        //when
+        gameMap.createGameMap(actorMatrix);
+
+        //then
+        assertEquals(emptyMap, outputStreamCaptor.toString());
+    }
+    @Test
+    void shouldPrintMapWithXXXOnSlant() {
+
+        //given
+        GameMap gameMap = new GameMap();
+        ActorMatrix actorMatrix = new ActorMatrix();
+        actorMatrix.addActorToMatrix(true, 11);
+        actorMatrix.addActorToMatrix(true, 22);
+        actorMatrix.addActorToMatrix(true, 33);
+
+        String emptyMap = """
+                X| |\s
+                -+-+-
+                 |X|\s
+                -+-+-
+                 | |X""";
+
+
+        //when
+        gameMap.createGameMap(actorMatrix);
 
         //then
         assertEquals(emptyMap, outputStreamCaptor.toString());
